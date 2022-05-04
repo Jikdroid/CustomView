887614ca9df83fda00de746d3a9dba7dc48f7e85
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-04 18:48:56 +0900

    #3 - CustomView 에서 editTextHintColor 미적용 해결방안 이유: 숨겨진 android 기본 속성이 CustomView 에 영향을 준다. 1. themes 에서 default 값을 설정

--- app/src/main/java/org/inu/customview/CustomView.kt
+++ app/src/main/java/org/inu/customview/CustomView.kt
@@ -3,24 +3,39 @@ package org.inu.customview
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class CustomView@JvmOverloads
-constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr:Int = 0) : ConstraintLayout(context, attrs,defStyleAttr){
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr:Int = 0) : LinearLayout(context, attrs,defStyleAttr){
     private val binding = View.inflate(context, R.layout.layout_customview, this)
 
     init {
-        val a = context.obtainStyledAttributes(attrs,R.styleable.customViewData)
-        val titleText = a.getString(R.styleable.customViewData_itemTitleText) ?: "Default"
-        val titleTextColor = a.getColor(R.styleable.customViewData_itemTitleTextColor, 0xFF1DE9B6.toInt())
-        val contentText = a.getString(R.styleable.customViewData_itemContentText) ?: "Default"
-        val contentTextColor = a.getColor(R.styleable.customViewData_itemContentTextColor, 0xFFFF0000.toInt())
+        // Load attributes
+        val a = context.obtainStyledAttributes(attrs,R.styleable.MyViewStyleable)
+        // customText로 정의한 문자열 혹은 Default 문자열
+        val customText = a.getString(R.styleable.MyViewStyleable_android_text) ?: "Default"
+        // customTextColor로 정의한 색상 혹은 #FF1DE9B6 컬러
+        val customTextColor = a.getColor(R.styleable.MyViewStyleable_android_textColor, 0xFFFFFFFF.toInt())
+        // customTextColor로 정의한 색상 혹은 #FFFF0000 컬러
+        val customTextColorHint = a.getColor(R.styleable.MyViewStyleable_android_textColorHint, 0xFFFFFFFF.toInt())
         a.recycle()
 
-        binding.findViewById<TextView>(R.id.title).text = titleText
-        binding.findViewById<TextView>(R.id.title).setTextColor(titleTextColor)
-        binding.findViewById<TextView>(R.id.content).text = contentText
-        binding.findViewById<TextView>(R.id.content).setTextColor(contentTextColor)
+
+        binding.findViewById<TextView>(R.id.title1).apply {
+            text = customText
+            setTextColor(customTextColor)
+            setHintTextColor(customTextColorHint)
+        }
+        binding.findViewById<TextView>(R.id.edit1).apply {
+            text = customText
+            setTextColor(customTextColor)
+            setHintTextColor(customTextColorHint)
+        }
+        binding.findViewById<TextView>(R.id.edit2).apply {
+            setTextColor(customTextColor)
+            setHintTextColor(customTextColorHint)
+        }
     }
 }
