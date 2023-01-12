f8ac639515cf38c35bab6304037b9f6b4149784b
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-01-12 20:37:27 +0900

    frame 구현

--- uniletter-spinner/app/src/main/java/org/boostcamp/uniletter_spinner/Jikpinner.kt
+++ uniletter-spinner/app/src/main/java/org/boostcamp/uniletter_spinner/Jikpinner.kt
@@ -2,30 +2,31 @@ package org.boostcamp.uniletter_spinner
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
-import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.FrameLayout
+import android.widget.ListPopupWindow
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.RecyclerView
 
 class Jikpinner @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    private lateinit var items: Array<CharSequence>
 
-    private lateinit var title: TextView
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var items: Array<CharSequence>
+    private val popUp = ListPopupWindow(context)
 
     init {
-        setAttrs(attrs)
-        setView()
-        setVisibility()
+        inflate(context, R.layout.jikpinner, this@Jikpinner)
 
-        recyclerView.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-            title.width = right - left
-        }
+
+        setAttrs(attrs)
+        setAdapter()
+        setView()
+        setPopup()
+        popUp.setContentWidth(measureWidth() + 100)
+        findViewById<TextView>(R.id.tv_title).width = popUp.width
     }
 
     private fun setAttrs(attrs: AttributeSet?) {
@@ -35,19 +36,50 @@ class Jikpinner @JvmOverloads constructor(
         }
     }
 
-    private fun setView() {
-        inflate(context, R.layout.jikpinner, this@Jikpinner)
-        recyclerView = findViewById<RecyclerView>(R.id.rv_menu).apply {
-            adapter = JikpinnerAdapter(items)
+
+    private fun setAdapter() {
+        popUp.setAdapter(ArrayAdapter(context, R.layout.item_jikpinner, items))
+    }
+
+    private fun measureWidth(): Int {
+        var width = 0
+        for (i in items.indices) {
+            val view = ArrayAdapter(context, R.layout.item_jikpinner, items).getView(
+                i,
+                null,
+                FrameLayout(context)
+            )
+            view.measure(0, 0)
+            width = width.coerceAtLeast(view.measuredWidth)
         }
-        title = findViewById<TextView>(R.id.title).apply {
+        return width
+    }
+
+    private fun setView() {
+        findViewById<TextView>(R.id.tv_title).apply {
             text = items.first()
+            setOnClickListener {
+                if (popUp.isShowing) {
+                    popUp.dismiss()
+                } else {
+                    popUp.show()
+                }
+            }
         }
     }
 
-    private fun setVisibility() {
-        title.setOnClickListener {
+    private fun setPopup() {
 
+        popUp.isModal = true
+        popUp.anchorView = this
+        popUp.verticalOffset = this.height + 8
+        setOnItemClickListener()
+    }
+
+    private fun setOnItemClickListener() {
+        popUp.setOnItemClickListener { _, _, position, _ ->
+            findViewById<TextView>(R.id.tv_title).text = items[position]
+            popUp.dismiss()
         }
     }
 }
